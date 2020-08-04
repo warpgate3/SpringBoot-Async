4a358112cc31a59e8e26139a46b05e98f89fdb5f
warpgate3 <warpgate3@naver.com>
2020-08-04 14:51:54 +0900

    async final

--- src/main/java/info/m2sj/asynctutorial/AsyncTutorialApplication.java
+++ src/main/java/info/m2sj/asynctutorial/AsyncTutorialApplication.java
@@ -1,19 +1,21 @@
 package info.m2sj.asynctutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-//@EnableAsync
 public class AsyncTutorialApplication {
+    private final static Logger log = LoggerFactory.getLogger(AsyncTutorialApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(AsyncTutorialApplication.class, args);
@@ -23,20 +25,38 @@ public class AsyncTutorialApplication {
     @RequestMapping("/")
     static class AsyncController {
 
-        @GetMapping("async/{name}")
-        public Callable<String> getName(@PathVariable String name) throws InterruptedException {
-            System.out.println("==> in the method");
+        @GetMapping("sync/{name}")
+        public String sync(@PathVariable String name) throws InterruptedException {
+            log.info(" ---> in sync");
+            TimeUnit.SECONDS.sleep(10);
+            return "[" + name + "]";
+        }
+
+        @GetMapping("callable/{name}")
+        public Callable<String> callable(@PathVariable String name) {
+            log.info(" ---> in callable");
             return () -> {
                 TimeUnit.SECONDS.sleep(10);
                 return "[" + name + "]";
             };
         }
 
-        @GetMapping("sync/{name}")
-        public String getName2(@PathVariable String name) throws InterruptedException {
-            System.out.println("==> in the method 2");
-            TimeUnit.SECONDS.sleep(10);
-            return "[" + name + "]";
+        @GetMapping("deffer/{name}")
+        public DeferredResult deferredResult(@PathVariable String name) {
+            log.info("==> in deferredResult");
+            DeferredResult<String> rtn = new DeferredResult<>();
+            Runnable anotherThread = () -> {
+                try {
+                    TimeUnit.SECONDS.sleep(10);
+                    rtn.setResult("[" + name + "]");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            new Thread(anotherThread).start();
+
+            return rtn;
         }
     }
 }
